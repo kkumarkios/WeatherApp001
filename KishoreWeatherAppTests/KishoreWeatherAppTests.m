63ea3d271d5d4d9cfc4b022bee47f7191ece8cb4
kishorekumarfacetime <kishorekumar@kishorekumarfacetimes-macbook-pro.local>
2016-09-13 19:58:54 -0400

    Initial Commit

---
+++ KishoreWeatherAppTests/KishoreWeatherAppTests.m
@@ -0,0 +1,39 @@
+//
+//  KishoreWeatherAppTests.m
+//  KishoreWeatherAppTests
+//
+//  Created by kishorekumarfacetime on 9/13/16.
+//  Copyright © 2016 kishorekumarfacetime. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KishoreWeatherAppTests : XCTestCase
+
+@end
+
+@implementation KishoreWeatherAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
